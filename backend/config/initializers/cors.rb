c77b1f2ceb3d21a065c73a09578808da8d52fbc6
riku-work10 <ukir19990530@gmail.com>
2025-03-07 02:15:54 +0900

    render-build.sh追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8000', '127.0.0.1:8000'
+    origins 'http://localhost:8000', '127.0.0.1:8000', 'https://render-vercel-sample.vercel.app/'
 
     resource "*",
       headers: :any,
